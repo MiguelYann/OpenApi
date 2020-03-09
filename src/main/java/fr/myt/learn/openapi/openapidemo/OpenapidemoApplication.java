90b43fdd8ee06477aa79632d3427fa7beebf0e14
miguel_tsamba <mtsamba@oui.sncf>
2020-03-09 15:25:51 +0100

    Setting up Spring Doc Open API with Swagger

---
+++ src/main/java/fr/myt/learn/openapi/openapidemo/OpenapidemoApplication.java
@@ -0,0 +1,13 @@
+package fr.myt.learn.openapi.openapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapidemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenapidemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/myt/learn/openapi/openapidemo/OpenapidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.myt.learn.openapi.openapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapidemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
